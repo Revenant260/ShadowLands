0ce4313b274b570c003cd44694ac679fefb66c32
Revenant260 <50692095+Revenant260@users.noreply.github.com>
2023-04-02 12:07:44 -0400

    derp

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ var socket = io();
 var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
+//var stores = []
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -13,11 +14,23 @@ form.addEventListener('submit', function (e) {
   }
 });
 
+window.onload = function() {
+  //var store = sessionStorage.getItem("msgs")
+ // console.log(store)
+ // var tmp = store.split("@|@")
+  //tmp.forEach(ele => {
+   // var item = document.createElement('li');
+   // item.textContent = ele
+   // messages.appendChild(item);
+ // });
+}
 
 socket.on('connect', () => {
   let userss = JSON.parse(localStorage.getItem('userp'))
   if (userss === null) {
     socket.emit('userData', "new")
+    stores.push("Joined Shadowlands")
+    stores = sessionStorage.setItem("msgs")
   } else {
     socket.emit('userData', userss)
     socket.emit('join', userss.room)
@@ -32,11 +45,12 @@ socket.on('update', (datas) => {
 socket.on('chat message', function (msg) {
   var item = document.createElement('li');
   item.textContent = msg;
+  //stores.push(msg)
+  //var tmp = stores.join("@|@")
+  //sessionStorage.setItem("msgs", tmp)
   messages.appendChild(item);
   messages.lastChild.scrollIntoView({ behavior: "smooth" })
-
   var local = JSON.parse(localStorage.getItem('userp')).usern + "|" + JSON.parse(localStorage.getItem('userp')).room + "|" + JSON.parse(localStorage.getItem('userp')).id
-
   if (item.textContent.includes(JSON.parse(localStorage.getItem('userp')).usern)) return
   if (item.textContent.includes("@active")) return socket.emit("active", local)
 });
